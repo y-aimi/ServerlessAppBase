a009b742a7fd0237591b192fe306a9019cd5cb84
yoshiki <yoshiki0885@gmail.com>
2022-11-30 21:04:32 +0900

    開発に必要なライブラリを導入

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    ...require(`./config/${process.env.APP_ENV || 'local'}.json`),
+  },
+  webpack: (config) => {
+    // パス解決のエイリアス設定
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config
+  },
+};
 
 module.exports = nextConfig
